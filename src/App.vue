6db081293dac2d3fae9bf1c8a972c20b5065df2e
Rifayudeen <q.rifayudeen@gmail.com>
2023-06-22 09:01:57 +0530

    eth account balance

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="bg-[#fafcff] absolute right-0 left-0 bottom-0 top-0">
+        <BalanceApp />
+    </main>
+</template>
+
+<script>
+
+import BalanceApp from './components/BalanceApp.vue'
+
+export default {
+    components:{
+        BalanceApp,
+    }
+}
+</script>
